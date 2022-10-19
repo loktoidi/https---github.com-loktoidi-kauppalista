cc01de5827a5c576af631b8dd4fbbb5fa5365131
loktoidi <loktoidi@luukku.com>
2022-10-19 13:04:46 +0300

    eka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 import './App.css';
 
+const URL = 'http://localhost/shoppinglist/';
+
 function App() {
+  const [items, setItems] = useState([])
+  const[newItem, setNewItem] = useState ('')
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response)=> {
+      setItems(response.data)
+    }).catch (error => {
+      console.log(error.response ? error.response.data.error : error)
+      alert('Haku ei onnistu');
+    })
+
+  },[])
+
+  const save = (e) => {
+    e.preventDefault()
+    const json = JSON.stringify({description:newItem})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    .then ((response) => {
+      setItems([...items,response.data])
+    }).catch(error => {
+      console.log(error.response ? error.response.data.error : error)
+      alert('Häiriö')
+    })
+    
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+      <label>Uusi tuote</label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)}/>
+      <button>Tallenna</button>
+      </form>
+      <h3>Minun tuotteet</h3>
+      <ol>
+        {
+          items?.map(item => (
+            <li key={item.id}>{item.description}</li>
+          ))}
+        
+        </ol>
     </div>
   );
 }
 
 export default App;
+
